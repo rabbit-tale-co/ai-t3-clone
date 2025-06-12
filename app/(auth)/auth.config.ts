19370ecdf6bd0143cfc0de66468bbf6ea5728ec5
poker-cats-co <hasiradoo@starowl.dev>
2025-06-12 23:13:21 +0200

    test

--- app/(auth)/auth.config.ts
+++ app/(auth)/auth.config.ts
@@ -3,28 +3,11 @@ import type { NextAuthConfig } from 'next-auth';
 export const authConfig = {
   pages: {
     signIn: '/login',
-    newUser: '/',
+    newUser: '/register',
   },
   providers: [
     // added later in auth.ts since it requires bcrypt which is only compatible with Node.js
     // while this file is also used in non-Node.js environments
   ],
   callbacks: {},
-  trustHost: true,
-  session: {
-    strategy: 'jwt',
-    maxAge: 30 * 24 * 60 * 60, // 30 dni
-  },
-  cookies: {
-    sessionToken: {
-      name: `__Secure-next-auth.session-token`,
-      options: {
-        httpOnly: true,
-        sameSite: 'lax',
-        path: '/',
-        secure: true,
-      },
-    },
-  },
-  debug: process.env.NODE_ENV === 'development',
 } satisfies NextAuthConfig;
