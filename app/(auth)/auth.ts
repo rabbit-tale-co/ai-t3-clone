7f14553406d0fc2bd5e424ebb778bc7fc998b931
poker-cats-co <hasiradoo@starowl.dev>
2025-06-13 11:06:11 +0200

    merge

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -105,13 +105,10 @@ export const {
 
       return session;
     },
-<<<<<<< Updated upstream
-=======
     async redirect({ url, baseUrl }) {
       if (url.startsWith('/')) return `${baseUrl}${url}`;
       else if (new URL(url).origin === baseUrl) return url;
       return baseUrl;
     },
->>>>>>> Stashed changes
   },
 });
